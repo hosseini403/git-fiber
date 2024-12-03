a6f2c05d5f9b28030461c511fcae43c2c6d43355
hosseini403 <sahelbarani91@gmail.com>
2024-12-03 18:26:29 +0330

    فایبر یک

---
+++ fiber1/main.go
@@ -0,0 +1,34 @@
+package main
+import (
+	"log"
+	"github.com/gofiber/fiber/v2"
+)
+func main() {
+	// Fiber instance
+	app := fiber.New()
+	// Routes
+	app.Get("/", hello)
+	// Start server
+	log.Fatal(app.Listen(":3001"))
+}
+// Handler
+func hello(c *fiber.Ctx) error {
+	return c.SendString("Hello, World 👋!")
+}
+package main
+import (
+	"log"
+	"github.com/gofiber/fiber/v2"
+)
+func main() {
+	// Fiber instance
+	app := fiber.New()
+	// Routes
+	app.Get("/", hello)
+	// Start server
+	log.Fatal(app.Listen(":3001"))
+}
+// Handler
+func hello(c *fiber.Ctx) error {
+	return c.SendString("Hello, World 👋!")
+}
